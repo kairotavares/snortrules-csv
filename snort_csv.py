b985c8e89ef41a6ae9d2d559e8f0458f9e021705
Kairo Tavares <kairo.ces.tavares@hpe.com>
2020-05-29 18:43:31 -0300

    Add options and options signature

--- snort_csv.py
+++ snort_csv.py
@@ -4,6 +4,8 @@ import sys
 from os import listdir
 from os.path import isfile, join
 
+all_options = []
+ignore_options = ['msg', 'metadata', 'classtype', 'sid', 'rev', 'reference', 'tag']
 ignore_list = ['snort3-deleted.rules']
 
 def is_rule(rule_line):
@@ -41,6 +43,17 @@ def to_row_value(row):
             values.append(value)
     _, msg = row.options[0]
     values.append(msg[0])
+    options = []
+    options_signature = []
+
+    for _, data in row.options.items():
+        t, d = data
+        if t not in ignore_options:
+            options.append(t)
+            options_signature.append(data)
+    all_options.extend(options)
+    values.append(str(options))
+    values.append(str(options_signature))
     return values
 
 def write_to_file(output_file, header, rows):
@@ -53,9 +66,9 @@ def write_to_file(output_file, header, rows):
 def process_single_file(rules_file, output_file):
     rules = read_rules(rules_file)
     header = list(get_keys(rules[0].header))
-    header.append('msg')
+    header.extend(['msg', 'options', 'options_signature'])
     write_to_file(output_file, header, rules)
-    print("Done")
+    print("Done. Processed {}".format(len(rules)))
 
 def process_multiple_files(rules_files, output_file):
     rules = []
@@ -65,9 +78,10 @@ def process_multiple_files(rules_files, output_file):
         rules.extend(file_rules)
     print(len(rules))
     header = list(get_keys(rules[0].header))
-    header.append('msg')
+    header.extend(['msg', 'options', 'options_signature'])
+
     write_to_file(output_file, header, rules)
-    print("Done")
+    print("Done. Processed {}".format(len(rules)))
 
 if __name__ == '__main__':
     rules_path = sys.argv[1]
@@ -79,3 +93,8 @@ if __name__ == '__main__':
         files = [join(rules_path, f) for f in listdir(rules_path)
                  if isfile(join(rules_path, f)) and '.rules' in f and f not in ignore_list]
         process_multiple_files(files, output_file)
+
+    #all = list(set(all_options))
+    #all.sort()
+    #print(len(all))
+    #[ print(a) for a in all]
